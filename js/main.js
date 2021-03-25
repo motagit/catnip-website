0e5a74032c98e52fe43afe4f355f67d0a3472194
motasjw <lucasm.lopes087@gmail.com>
2021-03-25 17:30:15 -0300

    Javascript animations

--- js/main.js
+++ js/main.js
@@ -10,3 +10,30 @@ $(".rslides_portfolio").responsiveSlides({
   timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
   pager: true,           // Boolean: Show pager, true or false
 });
+
+Visibility.onVisible(function() {
+	setTimeout(function() {
+		$(".introduction blockquote").addClass("animate__animated animate__fadeInDown");
+	}, 200);
+	setTimeout(function() {
+		$(".introduction .btn").addClass("animate__animated animate__fadeInDown");
+	}, 600);
+	setTimeout(function() {
+		$(".animar").addClass("animate__animated animate__fadeInLeft");
+	}, 1600);
+	setTimeout(function() {
+		$(".animar-port").addClass("animate__animated animate__fadeInUp");
+	}, 1000);
+
+
+		setTimeout(function() {
+		$(".animar-interno").addClass("animate__animated animate__fadeInLeft");
+	}, 1000);
+		setTimeout(function() {
+		$(".introduction-interna h1").addClass("animate__animated animate__fadeInDown");
+	}, 200);
+	setTimeout(function() {
+		$(".introduction-interna p").addClass("animate__animated animate__fadeInDown");
+	}, 600);
+});
+
